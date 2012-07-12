7020670f6846f30cce425549aaf2aeb5e140182f
Dave Mateer <davemateer@gmail.com>
2012-07-12 15:35:56 -0500

    changing structure of dirs

---
+++ Euler53/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class E53Tests
+    {
+        [Test]
+        public void MethodUnderTest_scenario_expectedbehaviour()
+        {
+            var result = E53.DoSomething();
+            Assert.AreEqual(1, result);
+        }
+    }
+
+    public class E53
+    {
+        public static int DoSomething()
+        {
+            return 1;
+        }
+    }
+}
